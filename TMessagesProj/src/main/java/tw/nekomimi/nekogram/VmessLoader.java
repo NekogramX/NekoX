ca9eceaf3634c5e96cbcd455419801f19ee5f7d5
風間わたる <i@wataru.moe>
2020-02-25 15:18:04 +0800

    喵

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/VmessLoader.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/VmessLoader.java
@@ -29,7 +29,85 @@ public class VmessLoader {
 
         String addr = InternalProxy.newAddress();
 
-        String cfg = "{\"inbounds\":[{\"listen\":\"127.0.0.1\",\"port\":11210,\"protocol\":\"socks\",\"settings\":{\"auth\":\"noauth\",\"udp\":true,\"userLevel\":8},\"sniffing\":{\"destOverride\":[\"http\",\"tls\"],\"enabled\":true}},{\"listen\":\"127.0.0.1\",\"port\":11211,\"protocol\":\"http\",\"settings\":{\"userLevel\":8}}],\"outbounds\":[{\"protocol\":\"vmess\",\"settings\":{\"vnext\":[{\"address\":\"_address\",\"port\":443,\"users\":[{\"alterId\":64,\"id\":\"73670f86-6046-4ffd-b468-6cd73cea1f29\",\"level\":8,\"security\":\"none\"}]}]},\"streamSettings\":{\"network\":\"ws\",\"security\":\"tls\",\"tlssettings\":{\"allowInsecure\":true,\"serverName\":\"kurumi.io\"},\"wssettings\":{\"connectionReuse\":true,\"headers\":{\"Host\":\"kurumi.io\"},\"path\":\"/internet\"}}}]}";
+        String cfg = "{\n" +
+                "  \"inbounds\": [\n" +
+                "    {\n" +
+                "      \"tag\": \"socks-in\",\n" +
+                "      \"listen\": \"127.0.0.1\",\n" +
+                "      \"port\": 11210,\n" +
+                "      \"protocol\": \"socks\",\n" +
+                "      \"settings\": {\n" +
+                "        \"auth\": \"noauth\",\n" +
+                "        \"udp\": true,\n" +
+                "        \"userLevel\": 8\n" +
+                "      },\n" +
+                "      \"sniffing\": {\n" +
+                "        \"destOverride\": [\n" +
+                "          \"http\",\n" +
+                "          \"tls\"\n" +
+                "        ],\n" +
+                "        \"enabled\": true\n" +
+                "      }\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"listen\": \"127.0.0.1\",\n" +
+                "      \"port\": 11211,\n" +
+                "      \"protocol\": \"http\",\n" +
+                "      \"settings\": {\n" +
+                "        \"userLevel\": 8\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"outbounds\": [\n" +
+                "    {\n" +
+                "      \"tag\": \"proxy\",\n" +
+                "      \"protocol\": \"vmess\",\n" +
+                "      \"settings\": {\n" +
+                "        \"vnext\": [\n" +
+                "          {\n" +
+                "            \"address\": \"_address\",\n" +
+                "            \"port\": 443,\n" +
+                "            \"users\": [\n" +
+                "              {\n" +
+                "                \"alterId\": 64,\n" +
+                "                \"id\": \"73670f86-6046-4ffd-b468-6cd73cea1f29\",\n" +
+                "                \"level\": 8,\n" +
+                "                \"security\": \"none\"\n" +
+                "              }\n" +
+                "            ]\n" +
+                "          }\n" +
+                "        ]\n" +
+                "      },\n" +
+                "      \"streamSettings\": {\n" +
+                "        \"network\": \"ws\",\n" +
+                "        \"security\": \"tls\",\n" +
+                "        \"tlssettings\": {\n" +
+                "          \"allowInsecure\": true,\n" +
+                "          \"serverName\": \"kurumi.io\"\n" +
+                "        },\n" +
+                "        \"wssettings\": {\n" +
+                "          \"connectionReuse\": true,\n" +
+                "          \"headers\": {\n" +
+                "            \"Host\": \"kurumi.io\"\n" +
+                "          },\n" +
+                "          \"path\": \"/internet\"\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"routing\": {\n" +
+                "    \"domainStrategy\": \"IPIfNonMatch\",\n" +
+                "    \"rules\": [\n" +
+                "      {\n" +
+                "        \"inboundTag\": [\n" +
+                "          \"socks-in\"\n" +
+                "        ],\n" +
+                "        \"outboundTag\": \"proxy\",\n" +
+                "        \"type\": \"field\"\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
 
         cfg = cfg.replace("_address", InternalProxy.newAddress());
 
