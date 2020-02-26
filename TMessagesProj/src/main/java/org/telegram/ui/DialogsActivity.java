001cd2c1cb11671c230f66cdc6749967f5910e73
風間わたる <i@wataru.moe>
2020-02-26 20:01:23 +0800

    喵

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -3334,6 +3334,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
         if (proxyDrawable == null) {
             return;
         }
+        /*
         SharedPreferences preferences = ApplicationLoader.applicationContext.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
         String proxyAddress = preferences.getString("proxy_ip", "");
         boolean proxyEnabled;
@@ -3341,12 +3342,16 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
             if (!actionBar.isSearchFieldVisible()) {
                 proxyItem.setVisibility(View.VISIBLE);
             }
-            proxyDrawable.setConnected(proxyEnabled, currentConnectionState == ConnectionsManager.ConnectionStateConnected || currentConnectionState == ConnectionsManager.ConnectionStateUpdating, animated);
-            proxyItemVisisble = true;
+
+         */
+            proxyDrawable.setConnected(true, currentConnectionState == ConnectionsManager.ConnectionStateConnected || currentConnectionState == ConnectionsManager.ConnectionStateUpdating, animated);
+           /* proxyItemVisisble = true;
         } else {
            // proxyItem.setVisibility(View.GONE);
             proxyItemVisisble = false;
         }
+
+            */
     }
 
     private void updateSelectedCount() {
