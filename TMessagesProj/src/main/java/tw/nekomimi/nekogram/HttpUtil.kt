4b1a84129fa31f64172bad7cdc8053ed78df4c86
風間わたる <i@wataru.moe>
2020-02-28 17:04:36 +0800

    saveProxyList

---
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/HttpUtil.kt
@@ -0,0 +1,25 @@
+package tw.nekomimi.nekogram
+
+import cn.hutool.http.HttpUtil
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+object HttpUtil {
+
+    val okhttpClient = OkHttpClient().newBuilder().build()
+
+    @JvmStatic
+    operator fun get(url: String): String {
+
+        val request = Request.Builder().url(url).build()
+
+        okhttpClient.newCall(request).execute().apply {
+
+            val body = body()
+
+            return body?.string() ?: error("HTTP ERROR ${code()}")
+
+        }
+
+    }
+}
