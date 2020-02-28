ba2020fc66201922237e2b308870e0848dca12ec
風間わたる <i@wataru.moe>
2020-02-29 00:08:46 +0800

    喵

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -3652,7 +3652,7 @@ public class MessagesController extends BaseController implements NotificationCe
             removeCurrent = 1;
         }
         lastCheckProxyId++;
-        if (!NekoConfig.hideProxySponsorChannel && enabled && !TextUtils.isEmpty(proxyAddress) && !TextUtils.isEmpty(proxySecret)) {
+        if (!NekoConfig.hideProxySponsorChannel && (SharedConfig.currentProxy == null || !SharedConfig.currentProxy.isInternal) && enabled && !TextUtils.isEmpty(proxyAddress) && !TextUtils.isEmpty(proxySecret)) {
             checkingProxyInfo = true;
             int checkProxyId = lastCheckProxyId;
             TLRPC.TL_help_getProxyData req = new TLRPC.TL_help_getProxyData();
